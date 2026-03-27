a9c02a325b8e5ade0e8244f5e1c3aa920abb5eb3
Elias Graf <elias@elias-graf.dev>
2026-03-27 20:38:33 +0100

    remove sys ex from the common message enum

--- src/message_system_common.rs
+++ src/message_system_common.rs
@@ -10,13 +10,14 @@ use std::fmt::Display;
 
 use crate::message_channel_voice::mask_data;
 
+pub const SYSEX_START: u8 = 0xF0;
+pub const SYSEX_END: u8 = 0xF7;
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 #[repr(u8)]
 pub enum Status {
     SongPositionPointer = 0xF2,
     SongSelect = 0xF3,
-    SysExEnd = 0xF7,
-    SysExStart = 0xF0,
     TuneRequest = 0xF6,
 }
 
@@ -33,8 +34,6 @@ impl TryFrom<u8> for Status {
         Ok(match value {
             v if v == Status::SongPositionPointer.into() => Status::SongPositionPointer,
             v if v == Status::SongSelect.into() => Status::SongSelect,
-            v if v == Status::SysExEnd.into() => Status::SysExEnd,
-            v if v == Status::SysExStart.into() => Status::SysExStart,
             v if v == Status::TuneRequest.into() => Status::TuneRequest,
             undefined => return Err(undefined),
         })
@@ -45,7 +44,6 @@ impl TryFrom<u8> for Status {
 pub enum Message {
     SongPositionPointer(u16),
     SongSelect(u8),
-    SysEx(Vec<u8>),
     TuneRequest,
 }
 
@@ -54,8 +52,6 @@ impl Message {
         match status {
             Status::SongPositionPointer => Self::content_song_position_pointer(bytes),
             Status::SongSelect => Self::content_song_select(bytes),
-            Status::SysExEnd => Err(ParseError::SysExEndBeforeStart),
-            Status::SysExStart => Self::content_sys_ex(bytes),
             Status::TuneRequest => Ok((0, Self::TuneRequest)),
         }
     }
@@ -83,27 +79,21 @@ impl Message {
 
         Ok((1, Self::SongSelect(mask_data(byte))))
     }
+}
 
-    fn content_sys_ex(bytes: &[u8]) -> Result<(usize, Self), ParseError> {
-        let pos_end = bytes
-            .iter()
-            .position(|b| b == &Status::SysExEnd.into())
-            .ok_or(ParseError::SysExEndMissing)?;
+#[derive(Debug, PartialEq)]
+pub struct SysEx(pub Vec<u8>);
 
-        // SAFETY: `pos_end` is obtained with `iter().position()` above, which guarantees: `0 <=
-        // pos_end < len()`
-        let content = unsafe { bytes.get_unchecked(..pos_end) };
-        let content: Vec<_> = content.iter().map(|b| mask_data(*b)).collect();
-
-        Ok((pos_end + 1, Self::SysEx(content)))
+impl SysEx {
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        let content: Vec<_> = bytes.iter().map(|b| mask_data(*b)).collect();
+        Self(content)
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
     DataNotEnoughBytes { available: usize, length: usize },
-    SysExEndBeforeStart,
-    SysExEndMissing,
 }
 
 impl Display for ParseError {
@@ -113,8 +103,6 @@ impl Display for ParseError {
                 f,
                 "data not enough bytes, length: {length}, available: {available}"
             ),
-            ParseError::SysExEndBeforeStart => write!(f, "sys ex end before start"),
-            ParseError::SysExEndMissing => write!(f, "sys ex end missing"),
         }
     }
 }
@@ -123,14 +111,16 @@ impl Display for ParseError {
 mod tests {
     use crate::{
         message_channel_voice::mask_data,
-        message_system_common::{Message, ParseError, Status},
+        message_system_common::{Message, ParseError, Status, SysEx},
     };
 
     #[test]
     fn status_undefined() {
+        assert_eq!(Status::try_from(0xF0), Err(0xF0));
         assert_eq!(Status::try_from(0xF1), Err(0xF1));
         assert_eq!(Status::try_from(0xF4), Err(0xF4));
         assert_eq!(Status::try_from(0xF5), Err(0xF5));
+        assert_eq!(Status::try_from(0xF7), Err(0xF7));
     }
 
     #[test]
@@ -141,16 +131,6 @@ mod tests {
     #[test]
     fn status_song_select() {
         assert_eq!(Status::try_from(0xF3), Ok(Status::SongSelect));
-    }
-
-    #[test]
-    fn status_sys_ex_end() {
-        assert_eq!(Status::try_from(0xF7), Ok(Status::SysExEnd));
-    }
-
-    #[test]
-    fn status_sys_ex_start() {
-        assert_eq!(Status::try_from(0xF0), Ok(Status::SysExStart));
     }
 
     #[test]
@@ -224,43 +204,26 @@ mod tests {
     }
 
     #[test]
-    fn sys_ex_end_before_start() {
-        assert_eq!(
-            Message::from_bytes(Status::SysExEnd, &[]),
-            Err(ParseError::SysExEndBeforeStart)
-        );
-    }
-
-    #[test]
-    fn sys_ex_end_missing() {
-        assert_eq!(
-            Message::from_bytes(Status::SysExStart, &[]),
-            Err(ParseError::SysExEndMissing)
-        );
-        assert_eq!(
-            Message::from_bytes(Status::SysExStart, &[0x01]),
-            Err(ParseError::SysExEndMissing)
-        );
-    }
-
-    #[test]
     fn sys_ex() {
         assert_eq!(
-            Message::from_bytes(
-                Status::SysExStart,
-                &[0x01, Status::SysExEnd.into()]
-            ),
-            Ok((2, Message::SysEx(vec![0x01]))),
+            SysEx::from_bytes(&[0x01, 0x02, 0x03]),
+            SysEx(vec![0x01, 0x02, 0x03]),
         );
+    }
+
+    #[test]
+    fn sys_ex_empty() {
         assert_eq!(
-            Message::from_bytes(
-                Status::SysExStart,
-                &[0xFF, 0xFF, Status::SysExEnd.into()]
-            ),
-            Ok((
-                3,
-                Message::SysEx(vec![mask_data(0xFF), mask_data(0xFF),])
-            )),
+            SysEx::from_bytes(&[]),
+            SysEx(vec![]),
+        );
+    }
+
+    #[test]
+    fn sys_ex_masks_data_bytes() {
+        assert_eq!(
+            SysEx::from_bytes(&[0xFF, 0x80]),
+            SysEx(vec![0x7F, 0x00]),
         );
     }
 
